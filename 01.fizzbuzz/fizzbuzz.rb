4ea19bf139eedd86d484528cc76fd62f0f226780
BoyMentaiko <uraban.caiman@gmail.com>
2021-04-06 16:29:10 +0900

    インデントを修正し、ブロック変数を修正しました

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,12 +1,11 @@
-a = 1..20
-a.each do |a|
-    if a % 15 == 0
-      puts "FizzBuzz"
-    elsif a % 5 == 0
-      puts "Buzz"
-    elsif a % 3 == 0
-      puts "Fizz"
-    else
-      puts a
-    end
+(1..20).each do |a|
+  if a % 15 == 0
+    puts "FizzBuzz"
+  elsif a % 5 == 0
+    puts "Buzz"
+  elsif a % 3 == 0
+    puts "Fizz"
+  else
+    puts a
   end
+end
